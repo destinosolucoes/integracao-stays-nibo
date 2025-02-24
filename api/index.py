a0fa1f9e6f09a0a95854a9b34e456039a3ef6ba7
Flavio Riper <flavioriper_andrade@hotmail.com>
2025-02-24 11:58:04 -0300

    refactor: simplify webhook reservation logic and logging

--- api/index.py
+++ api/index.py
@@ -4,7 +4,6 @@ from fastapi import FastAPI, Request, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 
 from .stays.index import get_reservation_report
-from .nibo.utils import check_special_booking
 from .nibo.transaction import send_transaction, update_transaction, delete_transaction, check_transaction_created
 from .utils import create_reservation_dto, calculate_expedia, create_request_log, create_log, validate_header
 from .constants import DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
@@ -41,13 +40,12 @@ async def webhook_reservation(request: Request, session: SessionDep):
     data = await request.json()
     track_log = []
 
-    if data["action"] in ["reservation.created", "reservation.modified", "reservation.deleted", "reservation.canceled"]:
-        create_request_log(data["_dt"],data["action"],data["payload"],session)
+    create_request_log(data["_dt"],data["action"],data["payload"],session)
 
     if not validate_header(request.headers):
         raise HTTPException(status_code=403)
 
-    if data["action"] == "reservation.created":
+    if data["action"] == "reservation.modified":
         reservation = data["payload"]
         track_log.append({"get_payload":reservation})
 
@@ -79,38 +77,20 @@ async def webhook_reservation(request: Request, session: SessionDep):
                     print("Erro ao criar operational_transaction")
                     print(operational_transaction)
                 
-                if reservation_dto["partner_name"] == "API booking.com" and check_special_booking(reservation_dto["partner_name"]):
+                if reservation_dto["partner_name"] == "API booking.com" and reservation["stats"]["_f_totalPaid"] == 0:
                     comission_transaction = send_transaction(reservation_dto, "comission")
                     track_log.append({"send_transaction_comission":comission_transaction})
 
                     if comission_transaction is False:
                         print("Erro ao criar comission_transaction")
                         print(comission_transaction)
-        
-    if data["action"] == "reservation.modified":
-        reservation = data["payload"]
-        track_log.append({"get_payload":reservation})
-
-        if reservation["type"] == "booked":
-            reservation_report = get_reservation_report(reservation)
-            track_log.append({"get_reservation_report":reservation_report})
-
-            if "partnerName" not in reservation_report:
-                reservation_report["partnerName"] = "website"
-            
-            reservation_dto = create_reservation_dto(reservation_report, reservation)
-            track_log.append({"create_reservation_dto":reservation_dto})
-
-            reservation_dto = calculate_expedia(reservation_dto)
-            track_log.append({"calculate_expedia":reservation_dto})
-
-            update_transactions = update_transaction(reservation_report, reservation_dto)
-            track_log.append({"update_transaction":update_transactions})
-            
-            if update_transactions is False:
-                print("Erro ao criar update_transaction")
-                print(update_transactions)
-
+            else:
+                update_transactions = update_transaction(reservation_report, reservation_dto)
+                track_log.append({"update_transaction":update_transactions})
+                
+                if update_transactions is False:
+                    print("Erro ao criar update_transaction")
+                    print(update_transactions)
         
     if data["action"] == "reservation.deleted" or data["action"] == "reservation.canceled":
         reservation = data["payload"]
@@ -127,6 +107,7 @@ async def webhook_reservation(request: Request, session: SessionDep):
             print(delete_transactions)
 
     
-    create_log(data["_dt"],data["action"],data["payload"],{"track_log":track_log},session)
+    if data["action"] in ["reservation.modified", "reservation.deleted", "reservation.canceled"]:
+        create_log(data["_dt"],data["action"],data["payload"],{"track_log":track_log},session)
 
     return {}
